2f230f3e3dbd0ef4aae117962604af7280a4d0fc
= <=>
2024-06-11 17:29:04 +0530

    new commit

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,10 +1,10 @@
 import pytest
 from selenium import webdriver
-from Utility import utility_file
+from Utility import read_config
 
 @pytest.fixture()
 def test_setup_and_setdown(request):
-    browser = utility_file.get_config("basic info", "browser")
+    browser = read_config.get_config("basic info", "browser")
     if browser == "chrome":
         driver = webdriver.Chrome()
     elif browser == "firefox":
@@ -12,7 +12,7 @@ def test_setup_and_setdown(request):
     else:
         raise ValueError(f"Unsupported browser: {browser}")
     driver.maximize_window()
-    url = utility_file.get_config("basic info", "url")
+    url = read_config.get_config("basic info", "url")
     driver.get(url)
     driver.implicitly_wait(5)
     request.cls.driver = driver
